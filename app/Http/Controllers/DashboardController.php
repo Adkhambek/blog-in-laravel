f42bad25059e887d4a6fe58e611a65d8ae86aa3a
Adham <muzaffarov.adham@gmail.com>
2022-07-26 17:56:30 +0500

    page views sum

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,7 +13,15 @@ class DashboardController extends Controller
         return view('admin.dashboard.index', [
             'posts' => Post::count(),
             'categories' => Category::count(),
-            'views' => 12
+            'views' => $this->intWithStyle(Post::sum('views'))
         ]);
+    }
+
+    public function intWithStyle($n)
+    {
+        if ($n < 1000) return $n;
+        $suffix = ['','k','M','G','T','P','E','Z','Y'];
+        $power = floor(log($n, 1000));
+        return round($n/(1000**$power),1,PHP_ROUND_HALF_EVEN).$suffix[$power];
     }
 }
